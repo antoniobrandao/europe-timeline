ac58f9c29333a05cff727949b860110aec85c3bb
Antonio Brandao <antoniobrandaodesign@gmail.com>
2024-04-29 22:26:10 +0100

    Show warning screen when viewport is not wide enough

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,17 @@ import Caret from '@/lib/components/Caret'
 export default function Home() {
   return (
     <main className="bg-black text-white h-screen">
-      <Caret />
-      <div className="grid grid-cols-12">
-        <Timelines />
-        <Sidebar />
+      <div className="h-screen flex lg:hidden justify-center items-center w-full">
+        <p className="text-white text-center max-w-48 text-sm relative top-[-40px]">
+          Your screen needs to be at least 1024px wide the view this experience
+        </p>
+      </div>
+      <div className="hidden lg:block">
+        <Caret />
+        <div className="grid grid-cols-12">
+          <Timelines />
+          <Sidebar />
+        </div>
       </div>
     </main>
   )
